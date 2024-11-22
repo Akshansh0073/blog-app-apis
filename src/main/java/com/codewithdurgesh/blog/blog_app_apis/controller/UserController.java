8463f38fda354d903c3c6b6c31db7e3f2b84ded3
Akshansh <sakshansh.bv@gmail.com>
2024-11-22 18:40:11 +0530

    Second Commit

--- src/main/java/com/codewithdurgesh/blog/blog_app_apis/controller/UserController.java
+++ src/main/java/com/codewithdurgesh/blog/blog_app_apis/controller/UserController.java
@@ -1,14 +1,21 @@
 package com.codewithdurgesh.blog.blog_app_apis.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.service.annotation.PutExchange;
 
+import com.codewithdurgesh.blog.blog_app_apis.payload.ApiResponse;
 import com.codewithdurgesh.blog.blog_app_apis.payload.UserDto;
 import com.codewithdurgesh.blog.blog_app_apis.service.UserService;
 
@@ -26,5 +33,32 @@ public class UserController {
 		return new ResponseEntity<>(savedUserDto,HttpStatus.CREATED);
 	}
 	
+	@PutMapping("/{userId}")
+	public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto,@PathVariable Integer userId) {
+		
+		UserDto updatedUser = userService.updateUser(userDto,userId);
+		return new ResponseEntity<>(updatedUser,HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/{userId}")
+	public ResponseEntity<ApiResponse> deleteUser(@RequestBody UserDto userDto,@PathVariable Integer userId) {
+		
+		userService.deleteUser(userId);
+		return new ResponseEntity<>(new ApiResponse("User Deleted Successfully",true),HttpStatus.OK);
+	}
+	
+	@GetMapping("/")
+    public ResponseEntity<List<UserDto>> getAllUsers (){
+		
+    	 List<UserDto> allUser = userService.getAllUser();
+    	 return new ResponseEntity<List<UserDto>>(allUser,HttpStatus.OK);
+     }
+	
+	@GetMapping("/{userId}")
+    public ResponseEntity<UserDto> getSingleUser (@PathVariable Integer userId){
+		
+    	UserDto userDto = userService.getUserById(userId);
+    	 return new ResponseEntity<UserDto>(userDto,HttpStatus.OK);
+     }
 
 }
---
+++ src/main/java/com/codewithdurgesh/blog/blog_app_apis/payload/ApiResponse.java
@@ -0,0 +1,26 @@
+package com.codewithdurgesh.blog.blog_app_apis.payload;
+
+public class ApiResponse {
+
+	String message;
+	boolean status;
+	
+	public ApiResponse(String message, boolean status) {
+		super();
+		this.message = message;
+		this.status = status;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public boolean isStatus() {
+		return status;
+	}
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	
+}
